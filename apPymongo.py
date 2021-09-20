b6f2ca1b98f1200c94b77c4d8e375605fb413e94
Daniel Araya <daraya2020@alu.uct.cl>
2021-09-19 22:49:34 -0300

    Funciones de importación y exportación Json en MongoDB
    Ya están completas las funciones de exportación e importación de los documentos de MongoDB.

--- apPymongo.py
+++ apPymongo.py
@@ -1,8 +1,23 @@
 from pymongo import MongoClient
 import json
+from bson.objectid import ObjectId
+
+def Export(Ip):
+    client = MongoClient('localhost')
+
+    db = client['Registro']
+
+    columna = db['Usuario']
+
+    x = columna.find_one({"_id":Ip})
+
+    x["_id"] = str(ObjectId())
+
+    with open("xd.json", "w") as fp:
+        json.dump(x, fp)
 
 #Funcion para subir archivos .json a MongoDB
-def jsonUp():
+def Import(Archivo):
 
     client = MongoClient('localhost')
 
@@ -10,12 +25,12 @@ def jsonUp():
 
     columna = db['Usuario']
 
-    with open('Usuario3.json') as file:
+    with open('Archivo') as file:
         file_data = json.load(file)
 
-    print(file_data)
     if isinstance(file_data, list):
-        columna.insert_many(file_data)  
+        columna.insert_many(file_data) 
+
     else:
         columna.insert_one(file_data)
     
@@ -24,7 +39,6 @@ def Up(columna, nombre, correo, password):
 
     #Revisa si el usuario existe...
     if(columna.find_one({"name": nombre}) != None):
-        print(columna.find_one({"name": nombre},{}))
         print("Usuario Existente...")
 
     #Revisa si el correo existe...
@@ -33,8 +47,7 @@ def Up(columna, nombre, correo, password):
 
     #Añade al usuario
     else:
-        bomba = columna.insert_one({"_id":columna.count_documents({})+1,"name": nombre, "correo": correo, "contra": password})
-        print(bomba.inserted_id)
+        bomba = columna.insert_one({"_id":str(ObjectId()),"name": nombre, "correo": correo, "contra": password})
         print("Agregado Correctamente...")
         
 #Conexion con MongoDB       
@@ -46,10 +59,16 @@ db = client['Registro']
 #Coleccion de la base de datos
 columna = db['Usuario']
 
+Export("6147d8897e7f3556a6b4d11d")
+#Import()
+
+#Up(columna, "Xalodfhjsaf4", "Jual.cfdnsom", "wdyastv")
 # Devolver $Oid
-#x = columna.find_one({"name": "Xalo5"},{})
+#x = columna.find_one({"name": "Xalo2312"},{})
 #print(x["_id"])
 
+#Creacion de Identificador 
+#str(ObjectId())
 
 
 
