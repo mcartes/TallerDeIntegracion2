2dadbddc5af280b966f0776514a3a7e360df0741
Daniel Araya <daraya2020@alu.uct.cl>
2021-10-02 01:25:12 -0300

    Corrección de algunas funciones y creación de las bases de datos
    Se corrigen algunas funciones que no funcionaban correctamente y se realiza el correcto almacenamiento de las bases de datos para los usuarios.

--- apPymongo.py
+++ apPymongo.py
@@ -18,14 +18,13 @@ def Export(Ip):
 
 #Funcion para subir archivos .json a MongoDB
 def Import(Archivo):
-
     client = MongoClient('localhost')
 
     db = client['Registro']
 
     columna = db['Usuario']
 
-    with open('Archivo') as file:
+    with open(Archivo, encoding = 'utf8') as file:
         file_data = json.load(file)
 
     if isinstance(file_data, list):
@@ -33,36 +32,76 @@ def Import(Archivo):
 
     else:
         columna.insert_one(file_data)
-    
-#Funcion para subir usuarios a MangoDB 
-def Up(columna, nombre, correo, password):
 
+def Acceso(user, password):
+    client = MongoClient('localhost')
+
+    db = client['Registro']
+
+    columna = db['Usuario']
+
+    dou = columna.find_one({"name": user})
+
+    if(dou != None):
+        if(dou["contra"] == password):
+            return("Acceso confirmado...")
+        else:
+            return("Contraseña incorrecta...")
+    else:
+        return("Usuario No Registrado...")
+     
+
+#Funcion para subir usuarios a MangoDB 
+def Registro(nombre, password):
+    client = MongoClient('localhost')
+
+    db = client['Registro']
+
+    columna = db['Usuario']
     #Revisa si el usuario existe...
     if(columna.find_one({"name": nombre}) != None):
         print("Usuario Existente...")
 
     #Revisa si el correo existe...
-    elif(columna.find_one({"correo": correo}) != None):
-        print("Correo Existente...")
+    # elif(columna.find_one({"correo": correo}) != None):
+    #     print("Correo Existente...")
 
     #Añade al usuario
     else:
-        bomba = columna.insert_one({"_id":str(ObjectId()),"name": nombre, "correo": correo, "contra": password})
-        print("Agregado Correctamente...")
-        
+        a = columna.insert_one({"_id":str(ObjectId()),"name": nombre, "contra": password})
+        print("Agregado Correctamente el Usuario: ", nombre)
+        dou = columna.find_one({"name": nombre})
+        db = client[dou["_id"]]
+        columna = db["Documents"]
+        columna.insert_one({"_id":str(ObjectId()),"Bienvenida": "Un gusto recibirte"})
+
+
+def consultar(a):
+    client = MongoClient('localhost')
+
+    db = client['Registro']
+
+    columna = db['Usuario']
+
+    x = columna.find(a)
+    
+    print(str(list(x)))
+
 #Conexion con MongoDB       
-client = MongoClient('localhost')
+#client = MongoClient('localhost')
+#ad = input("Ingrese: ")
+#consultar(ad)
 
-#Base de datos
-db = client['Registro']
+# #Base de datos
+#db = client['Registro']
 
-#Coleccion de la base de datos
-columna = db['Usuario']
+# #Coleccion de la base de datos
+#columna = db['Usuario']
 
-Export("6147d8897e7f3556a6b4d11d")
-#Import()
+#Export("6147d8897e7f3556a6b4d11d")
+#Import("jsontaller2.json")
 
-#Up(columna, "Xalodfhjsaf4", "Jual.cfdnsom", "wdyastv")
+#Up(columna, "Ññ", "Jfdnsom", "wastv")
 # Devolver $Oid
 #x = columna.find_one({"name": "Xalo2312"},{})
 #print(x["_id"])
--- test_flask.py
+++ test_flask.py
@@ -1,6 +1,5 @@
-from flask import Flask
-from flask import render_template
-
+from flask import Flask, render_template, request
+import apPymongo as xd
 #variables
 
 usuario = ""
@@ -17,11 +16,40 @@ def index():
 @app.route("/login")
 def login():
     return render_template("login.html")
+    
+@app.route('/authenticate', methods=['POST'])
+def Inicio():
+    user = request.form['username']
+    password = request.form['password']
+
+    return xd.Acceso(user, password)
 
 @app.route("/singup")
 def signup():
     return render_template("signup.html")
-    
+
+@app.route('/register', methods=['POST'])
+def Registro():
+    user = request.form['username']
+    password = request.form['password']
+    xd.Registro(user, password)
+    return "<h1> Usuario agregado <h1>"
+
+@app.route('/sii', methods=['POST'])
+def usuario():
+    con = request.form['consulta']
+    con = con.replace("'","")
+    con = con.replace('"',"")
+    con = con.replace("","")
+    con = con.replace("{","")
+    con = con.replace("}","")
+    con = con.replace(" ","")
+    con = con.strip()
+    con = con.split(":")
+
+    #REQUIERE MODIFICACIÓN 
+    # user = db.Usuario.find({con[0]:con[1]})
+    # return str(list(user))
 '''
 @app.route("/edit")
 def edicion():
